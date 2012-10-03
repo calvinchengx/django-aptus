d518acb5596976775fdcfa6962e2a1854c0c2810
Calvin Cheng <linchuan.cheng@gmail.com>
2012-10-04 00:29:53 +0800

    Forget it. Use distribute instead.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from disutils.core import setup
+from setuptools import setup
 
 setup(
     name='Django Aptus',
-    version='0.0.3',
+    version='0.0.4',
     packages=['aptus', ],
     license='LICENSE',
     description='useful django functionality',
     long_description=open('README.md').read(),
     author='Calvin Cheng',
     author_email='calvin@calvinx.com',
-    install_requires=['Disutils', ]
+    install_requires=['distribute', ]
 )
