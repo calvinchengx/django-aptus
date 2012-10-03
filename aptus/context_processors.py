e0a3a926a0f2c4b9eba027956e24fc6b3289dd6a
Calvin Cheng <linchuan.cheng@gmail.com>
2012-10-04 00:42:40 +0800

    Typo

--- aptus/context_processors.py
+++ aptus/context_processors.py
@@ -4,7 +4,7 @@ from django.contrib.auth import REDIRECT_FIELD_NAME
 
 
 def login_url_with_redirect(request):
-    login_url = getattr(settings.LOGIN_URL, '/accounts/signin/')
+    login_url = getattr(settings, 'LOGIN_URL', '/accounts/signin/')
     path = urlquote(request.get_full_path())
     url = '%s?%s=%s' % (login_url, REDIRECT_FIELD_NAME, path)
     return {'login_url': url}
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Django Aptus',
-    version='0.0.4',
+    version='0.0.5',
     packages=['aptus', ],
     license='LICENSE',
     description='useful django functionality',
